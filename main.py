8e48b991361c3e3a6a0e003cced7623b4a4da7da
Unknown <allium.fistulosum.39@gmail.com>
2018-11-17 03:53:11 +0900

    benchmark

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import numpy as np
 from jggsystem import JGGSystem
+from gaqsystem import GAQSystem
+from individual import Individual
+import crossoverer
 from problem.frontier.sphere import sphere
 from problem.frontier.ellipsoid import ellipsoid
 
+def gaq_op(x):
+	x.sort(key=lambda i: i.fitness)
+	return crossoverer.rex(x[:n + 1])
+
+def gaq2_op(x):
+	x.sort(key = lambda i: i.fitness)
+	parents = x[:n]
+	x.sort(key = lambda i: i.birth_year if i.birth_year is not None else -1)
+	elders = x[:int(len(x) * 0.1)]
+	elders.sort(key = lambda i: i.fitness)
+	parents.extend(elders[:1])
+	return crossoverer.rex(parents)
+
 n = 20
-system = JGGSystem(ellipsoid, n, 6 * n, n + 1, 6 * n)
+problem = sphere
+jggsys = JGGSystem(problem, n, 6 * n, n + 1, 6 * n)
+gaqsys = GAQSystem(problem, 0, [Individual(n) for i in range(15 * n)], gaq_op)
+gaq2sys = GAQSystem(problem, 0, [Individual(n) for i in range(15 * n)], gaq2_op)
 
-system.step(33800)
+jggsys.step(27200)
+print(jggsys.get_best_individual());
 
-print(system.get_best_evaluation_value());
+gaqsys.step(27200)
+print(gaqsys.get_best_individual());
+
+gaq2sys.step(27200)
+print(gaq2sys.get_best_individual());
