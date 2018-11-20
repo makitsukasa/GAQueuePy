0aced15220d0ecd554c40a3b6f6495c9846bc851
Unknown <allium.fistulosum.39@gmail.com>
2018-11-21 06:00:01 +0900

    benchmark

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ from problem.frontier.ackley import ackley
 from problem.frontier.rastrigin import rastrigin
 from plot import plot
 
-def gaq_op(x):
+def gaq_plain_op(x):
 	x.sort(key=lambda i: i.fitness)
 	return crossoverer.rex(x[:n + 1])
 
-def gaq2_op(x):
+def gaq_always_random_op(x):
+	x.sort(key = lambda i: i.fitness)
+	parents = x[:n - 1]
+	for i in range(2):
+		rand = np.random.randint(len(x))
+		parents.append(x[rand])
+	return crossoverer.rex(parents)
+
+def gaq_rarely_random_op(x):
+	if np.random.random() > 0.5:
+		return gaq_always_random_op(x)
+	else:
+		return gaq_plain_op(x)
+
+def gaq_fixed_range_op(x):
 	x.sort(key = lambda i: i.fitness)
 	parents = x[:n - 1]
 	clone = x[:]
@@ -24,12 +38,14 @@ def gaq2_op(x):
 	parents.extend(clone[:2])
 	return crossoverer.rex(parents)
 
-def gaq3_op(x):
+def gaq_random_range_op(x):
 	x.sort(key = lambda i: i.fitness)
 	parents = x[:n - 1]
-	for i in range(2):
-		rand = np.random.randint(len(x))
-		parents.append(x[rand])
+	clone = x[:]
+	clone.sort(key = lambda i: -i.birth_year)
+	clone = clone[:len(x) * np.random.randint(1, 100) // 100]
+	clone.sort(key = lambda i: i.fitness)
+	parents.extend(clone[:2])
 	return crossoverer.rex(parents)
 
 n = 20
@@ -38,34 +54,33 @@ npar = n + 1
 nchi = 6 * n
 step_count = 27200
 problem = sphere
+gaqsystem_opt_list = [
+	["plain", "m"],
+	["always_random", "b"],
+	["rarely_random", "navy"],
+	["fixed_range", "c"],
+	["random_range", "g"],
+]
+randseed = np.random.randint(0x7fffffff)
 
+np.random.seed(randseed)
 jggsys = JGGSystem(problem, n, npop, npar, nchi)
 jggsys.step(step_count)
 jggsys.calc_raw_fitness(problem)
 best = jggsys.get_best_individual()
 print(best);
-plot(step_count, jggsys.history, fmt = 'r-', label = 'JGG : {:.10f}'.format(best.fitness))
+plot(step_count, jggsys.history, color = 'r', label = 'JGG : {:.10f}'.format(best.fitness))
 
-gaqsys = GAQSystem(problem, 0, [Individual(n) for i in range(npop)], gaq_op)
-gaqsys.step(step_count)
-gaqsys.calc_raw_fitness(problem)
-best = gaqsys.get_best_individual()
-print(best);
-plot(step_count, gaqsys.history, fmt = 'm-', label = 'GAQ : {:.4f}'.format(best.fitness))
-
-gaq2sys = GAQSystem(problem, 0, [Individual(n) for i in range(npop)], gaq2_op)
-gaq2sys.step(step_count)
-gaq2sys.calc_raw_fitness(problem)
-best = gaq2sys.get_best_individual()
-print(best);
-plot(step_count, gaq2sys.history, fmt = 'c-', label = 'GAQ2 : {:.4f}'.format(best.fitness))
-
-gaq3sys = GAQSystem(problem, 0, [Individual(n) for i in range(npop)], gaq3_op)
-gaq3sys.step(step_count)
-gaq3sys.calc_raw_fitness(problem)
-best = gaq3sys.get_best_individual()
-print(best);
-plot(step_count, gaq3sys.history, fmt = 'b-', label = 'GAQ3 : {:.4f}'.format(best.fitness))
+for opt in gaqsystem_opt_list:
+	name, color = opt
+	exec("op = gaq_{}_op".format(name))
+	np.random.seed(randseed)
+	gaq_sys = GAQSystem(problem, 0, [Individual(n) for i in range(npop)], op)
+	gaq_sys.step(step_count)
+	gaq_sys.calc_raw_fitness(problem)
+	best = gaq_sys.get_best_individual()
+	print(best);
+	plot(step_count, gaq_sys.history, color = color, label = 'GAQ_{} : {:.10f}'.format(name, best.fitness))
 
 plt.axis(xmin = 0, ymin = 0)
 plt.title('{f}(D{d}), {npop},{npar},{s}'.format(f = problem.__name__, d = n, npop = npop, npar = npar, s = step_count))
