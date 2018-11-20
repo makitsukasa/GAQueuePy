3c730dede694c1e637a7b05c5e2140fe80218dc2
Unknown <allium.fistulosum.39@gmail.com>
2018-11-21 05:59:32 +0900

    fix : plot use color

--- plot.py
+++ plot.py
@@ -5,11 +5,11 @@ import matplotlib as mpl
 font = {"family":"Noto Sans JP Regular"}
 mpl.rc('font', **font)
 
-def plot(step_count, history, fmt = 'b-', label = ''):
+def plot(step_count, history, color = 'b', label = ''):
 	x = range(step_count)
 	v = step_count // 100
 	np.random.shuffle(history)
 	history.sort(key = lambda i: i.birth_year if i.birth_year is not None else -1)
 	f_raw = [i.fitness for i in history]
 	f = np.convolve(f_raw, np.ones(v) / v, mode = 'vaild')
-	plt.plot(x[:len(f)], f, fmt, label = label)
+	plt.plot(x[:len(f)], f, color = color, label = label)
