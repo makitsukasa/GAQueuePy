b506c3ed3bf6228d031c50d90185d248b3c908f3
Unknown <allium.fistulosum.39@gmail.com>
2019-01-10 03:46:39 +0900

    bench

---
+++ bench.py
@@ -0,0 +1,167 @@
+import numpy as np
+from jggsystem import JGGSystem
+from gaqsystem import GAQSystem
+from swapsystem import SwapSystem
+from swapsystem2 import SwapSystem2
+from individual import Individual, State
+import crossoverer
+from problem.frontier.sphere import sphere
+from problem.frontier.ellipsoid import ellipsoid
+from problem.frontier.ktablet import ktablet
+from problem.frontier.rosenbrock import rosenbrock
+from problem.frontier.bohachevsky import bohachevsky
+from problem.frontier.ackley import ackley
+from problem.frontier.schaffer import schaffer
+from problem.frontier.rastrigin import rastrigin
+from problem.gmm.gmm import gmm, rough_gmm_ave, rough_gmm_weighted_ave, init_rough_gmm
+import warnings
+
+warnings.simplefilter("error", RuntimeWarning)
+
+def gaq_op_plain(x):
+	x.sort(key = lambda i : i.fitness)
+	parents = x[:npar]
+	for p in parents:
+		p.state = State.USED_IN_GAQ
+	return crossoverer.rex(parents, nchi)
+
+def gaq_op_plain_origopt(x):
+	x.sort(key = lambda i : i.fitness)
+	parents = x[:n + 1]
+	for p in parents:
+		p.state = State.USED_IN_GAQ
+	return crossoverer.rex(parents)
+
+def choose_population_throw_gaq(sys):
+	sys.history.sort(key = lambda i : i.birth_year)
+	return sys.history[:npop]
+
+def choose_population_replace_by_elites(sys, elites_count):
+	sys.history.sort(key = lambda i : i.birth_year)
+	initial = sys.history[:npop]
+	np.random.shuffle(sys.history)
+	ret = []
+	for i in initial:
+		if elites_count > 0 and i.state == State.USED_IN_GAQ:
+			elites_count -= 1
+		else:
+			ret.append(i)
+	sys.history.sort(key = lambda i : i.fitness)
+	ret.extend(sys.history[:elites_count])
+	return ret
+
+def init():
+	init_rough_gmm()
+
+best_lists = {}
+n = 20
+npar = n + 1
+loop_count = 1
+problem_list = [
+	{"problem_name" : "sphere", "problem" : sphere, "step" : 27200, "npop" : 6 * n, "nchi" : 6 * n},
+	{"problem_name" : "ellipsoid", "problem" : ellipsoid, "step" : 33800, "npop" : 6 * n, "nchi" : 6 * n},
+	{"problem_name" : "k-tablet", "problem" : ktablet, "step" : 48000, "npop" : 7 * n, "nchi" : 6 * n},
+	{"problem_name" : "rosenbrock", "problem" : rosenbrock, "step" : 157000, "npop" : 15 * n, "nchi" : 8 * n},
+	{"problem_name" : "bohachevsky", "problem" : bohachevsky, "step" : 33800, "npop" : 6 * n, "nchi" : 6 * n},
+	{"problem_name" : "ackley", "problem" : ackley, "step" : 55400, "npop" : 6 * n, "nchi" : 6 * n},
+	# {"problem_name" : "schaffer", "problem" : schaffer, "step" : 229000, "npop" : 9 * n, "nchi" : 8 * n},
+	{"problem_name" : "rastrigin", "problem" : rastrigin, "step" : 220000, "npop" : 22 * n, "nchi" : 8 * n},
+]
+
+for problem_info in problem_list:
+
+	best_list = {}
+	problem = problem_info["problem"]
+	raw_problem = problem_info["problem"]
+	problem_name = problem_info["problem_name"]
+	npop = problem_info["npop"]
+	nchi = problem_info["nchi"]
+	step_count = problem_info["step"]
+
+	for _ in range(loop_count):
+		randseed = np.random.randint(0x7fffffff)
+
+		init()
+		np.random.seed(randseed)
+		jgg_sys = JGGSystem(problem, n, npop, npar, nchi)
+		jgg_sys.step(step_count)
+		jgg_sys.calc_raw_fitness(raw_problem)
+		best = jgg_sys.get_best_individual()
+		if "jgg" in best_list:
+			best_list["jgg"] += best.raw_fitness / loop_count
+		else:
+			best_list["jgg"] = best.raw_fitness / loop_count
+
+		init()
+		np.random.seed(randseed)
+		gaq_sys = GAQSystem(problem, 0, [Individual(n) for i in range(npop)], gaq_op_plain_origopt)
+		gaq_sys.step(step_count)
+		gaq_sys.calc_raw_fitness(raw_problem)
+		best = gaq_sys.get_best_individual()
+		if "gaq" in best_list:
+			best_list["gaq"] += best.raw_fitness / loop_count
+		else:
+			best_list["gaq"] = best.raw_fitness / loop_count
+
+		init()
+		np.random.seed(randseed)
+		swap_sys = SwapSystem(problem, n, npop, npar, nchi)
+		swap_sys.gaq_sys.op = gaq_op_plain_origopt
+		swap_sys.switch_to_gaq = lambda sys : False
+		swap_sys.choose_population_to_jgg = choose_population_throw_gaq
+		swap_sys.step(step_count)
+		swap_sys.calc_raw_fitness(raw_problem)
+		best = swap_sys.get_best_individual()
+		if "throw_gaq" in best_list:
+			best_list["throw_gaq"] += best.raw_fitness / loop_count
+		else:
+			best_list["throw_gaq"] = best.raw_fitness / loop_count
+
+		init()
+		np.random.seed(randseed)
+		swap_sys = SwapSystem(problem, n, npop, npar, nchi)
+		swap_sys.gaq_sys.op = gaq_op_plain_origopt
+		swap_sys.switch_to_gaq = lambda sys : False
+		swap_sys.choose_population_to_jgg = lambda sys : choose_population_replace_by_elites(sys, npar)
+		swap_sys.step(step_count)
+		swap_sys.calc_raw_fitness(raw_problem)
+		best = swap_sys.get_best_individual()
+		if "replace" in best_list:
+			best_list["replace"] += best.raw_fitness / loop_count
+		else:
+			best_list["replace"] = best.raw_fitness / loop_count
+
+		init()
+		np.random.seed(randseed)
+		swap_sys = SwapSystem2(problem, n, npop, npar, nchi)
+		swap_sys.gaq_sys.op = gaq_op_plain_origopt
+		swap_sys.switch_to_gaq = lambda sys : False
+		swap_sys.choose_population_to_jgg = lambda sys : choose_population_replace_by_elites(sys, npar)
+		swap_sys.step(step_count)
+		swap_sys.calc_raw_fitness(raw_problem)
+		best = swap_sys.get_best_individual()
+		if "replace_2" in best_list:
+			best_list["replace_2"] += best.raw_fitness / loop_count
+		else:
+			best_list["replace_2"] = best.raw_fitness / loop_count
+
+	best_lists[problem_name] = best_list
+
+method_names = list(list(best_lists.values())[0].keys())
+
+print("loop", loop_count)
+
+print("(x10^10)|", end = "")
+for method_name in method_names:
+	print(method_name, "|", end = "")
+print()
+print("|", end = "")
+for method_name in method_names:
+	print("|", end = "")
+print()
+
+for problem_name, bests in best_lists.items():
+	print(problem_name, "|", end = "")
+	for method_name in method_names:
+		print(round(bests[method_name] * 10000000000, 0), "|", end = "")
+	print()
