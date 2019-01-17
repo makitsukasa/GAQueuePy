0008760cc614482feacf8c076b31a1f23ad7ec3b
Unknown <allium.fistulosum.39@gmail.com>
2019-01-18 03:54:38 +0900

    refact : problems

--- problem/frontier/ellipsoid.py
+++ problem/frontier/ellipsoid.py
@@ -5,5 +5,7 @@ def ellipsoid(x):
 	n = len(x)
 	ret = 0
 	for i in range(n):
-		ret += 10.0 ** (6.0 * i / (n - 1)) * shifted[i] ** 2
+		# ret += 10 ** (6.0 * i / (n - 1)) * shifted[i] ** 2
+		# ret += (1000 ** (i * 1.0 / (n - 1)) * shifted[i]) ** 2
+		ret += np.power(np.power(1000, float(i) / (n - 1)) * shifted[i], 2)
 	return ret
--- problem/frontier/ktablet.py
+++ problem/frontier/ktablet.py
@@ -3,4 +3,4 @@ import numpy as np
 def ktablet(x):
 	shifted = x * 10.24 - 5.12
 	k = len(x) // 4
-	return np.sum(shifted[:k] ** 2) + np.sum((shifted[k:] * 100) ** 2)
+	return np.sum(shifted[:k] ** 2) + np.sum((100.0 * shifted[k:]) ** 2)
