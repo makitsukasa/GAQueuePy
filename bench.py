09ee6bfe0e4d96898ebbf349e56bbf152f93d525
Unknown <allium.fistulosum.39@gmail.com>
2019-02-25 17:54:14 +0900

    bench

--- bench.py
+++ bench.py
@@ -104,17 +104,17 @@ steps_list = {}
 
 n = 20
 npar = n + 1
-loop_count = 1
+loop_count = 30
 goal = 1e-7
 problem_list = [
-	# {"problem_name" : "sphere", "problem" : sphere, "step" : 27200, "npop" : 6 * n, "nchi" : 6 * n},
+	{"problem_name" : "sphere", "problem" : sphere, "step" : 27200, "npop" : 6 * n, "nchi" : 6 * n},
 	# {"problem_name" : "ellipsoid", "problem" : ellipsoid, "step" : 33800, "npop" : 6 * n, "nchi" : 6 * n},
-	# {"problem_name" : "k-tablet", "problem" : ktablet, "step" : 48000, "npop" : 8 * n, "nchi" : 6 * n},
+	{"problem_name" : "k-tablet", "problem" : ktablet, "step" : 48000, "npop" : 8 * n, "nchi" : 6 * n},
 	# {"problem_name" : "rosenbrock", "problem" : rosenbrock, "step" : 157000, "npop" : 15 * n, "nchi" : 8 * n},
-	# {"problem_name" : "bohachevsky", "problem" : bohachevsky, "step" : 33800, "npop" : 6 * n, "nchi" : 6 * n},
-	# {"problem_name" : "ackley", "problem" : ackley, "step" : 55400, "npop" : 8 * n, "nchi" : 6 * n},
+	{"problem_name" : "bohachevsky", "problem" : bohachevsky, "step" : 33800, "npop" : 6 * n, "nchi" : 6 * n},
+	{"problem_name" : "ackley", "problem" : ackley, "step" : 55400, "npop" : 8 * n, "nchi" : 6 * n},
 	{"problem_name" : "schaffer", "problem" : schaffer, "step" : 229000, "npop" : 10 * n, "nchi" : 8 * n},
-	# {"problem_name" : "rastrigin", "problem" : rastrigin, "step" : 220000, "npop" : 24 * n, "nchi" : 8 * n},
+	{"problem_name" : "rastrigin", "problem" : rastrigin, "step" : 220000, "npop" : 24 * n, "nchi" : 8 * n},
 ]
 
 for problem_info in problem_list:
@@ -129,8 +129,8 @@ for problem_info in problem_list:
 	# step_count = problem_info["step"] // 10
 	# step_count = 100 * n
 	step_count = 300000
-	t = 10e-7
-	print(problem_name, "step:", step_count)
+	t = 1e-1
+	print(problem_name, "step:", step_count, ",t:", t)
 
 	for _ in range(loop_count):
 		randseed = np.random.randint(0x7fffffff)
